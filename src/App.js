4c5a9e996fb149ab738b3899bc79c763145962e3
Gill Peacegood <gill.peacegood@payliquid.com>
2018-03-19 09:27:14 +0000

    Add eliza

--- src/App.js
+++ src/App.js
@@ -1,75 +1,50 @@
-// @flow
-
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import questionaire from './questionaire';
-import Sound from 'react-sound';
-import type {Response} from './questionaire';
 
-function AnswerList ({answers, onAnswerChosen}: {answers: Array<Response>, onAnswerChosen: (nextQuestion: string)=>void}) {
-  const listItems = answers.map((answer) =>
-    <li onClick={() => onAnswerChosen(answer.nextQuestion)}>{answer.text}</li>
-  );
-  return (
-    <ul className="Answer-list">
-      {listItems}
-    </ul>
-  )
-}
+import ElizaBot from 'elizabot';
+import Speak from 'mespeak';
 
-class Question extends Component {
-  state = {
-    question: 'a',
-    soundStatus: Sound.status.PLAYING
-  };
+var eliza = new ElizaBot();
 
-  handleAnswerChosen = (question) => {
-    this.setState({question});
+class App extends Component {
+
+
+  constructor(props) {
+    super(props);
+    const question = eliza.getInitial();
+    this.state = {question, answer: ""};
   }
 
-  handleSongLoading = () => {
-
+  handleChange = (event) => {
+    this.setState({answer: event.target.value});
   }
 
-  handleSongPlaying = () => {
-
-  }
-
-  handleSongFinishedPlaying = () => {
+  handleSubmit = (event) => {
+    this.setState({question: eliza.transform(this.state.answer)});
+    event.preventDefault();
   }
 
   render() {
-    const question = questionaire[this.state.question]
-    const soundFile = `${process.env.PUBLIC_URL}/sounds/${question.sound}`
+    const {question, answer} = this.state;
     return (
-
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{question.text}</h1>
+          <h1 className="App-title">Welcome to Eliza</h1>
         </header>
-        <p className="App-intro">
-          <Sound
-             url={soundFile}
-             playStatus={this.state.soundStatus}
-             playFromPosition={1}
-             volume={100}
-             onLoading={this.handleSongLoading}
-             onPlaying={this.handleSongPlaying}
-             onFinishedPlaying={this.handleSongFinishedPlaying}
-           />
-           <AnswerList answers={question.responses} onAnswerChosen={this.handleAnswerChosen}/>
+        <p>
+          {question}
         </p>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Answer:
+            <input value={answer} onChange={this.handleChange}/>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
 
       </div>
     );
-  }
-}
-
-class App extends Component {
-  render() {
-    return <Question/>
   }
 }
 
